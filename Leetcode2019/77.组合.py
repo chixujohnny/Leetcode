d5cb0444913c57cca05aef7f34becd48c86ab0f2
阿煦 <chixu.cx@antfin.com>
2019-01-17 14:38:38 +0800

    2019年刷题

---
+++ Leetcode2019/77.组合.py
@@ -0,0 +1,52 @@
+# coding: utf-8
+
+# 给定两个整数 n 和 k，返回 1 ... n 中所有可能的 k 个数的组合。
+# 示例:
+# 输入: n = 4, k = 2
+# 输出:
+# [
+#   [2,4],
+#   [3,4],
+#   [2,3],
+#   [1,2],
+#   [1,3],
+#   [1,4],
+# ]
+
+# 这个题目是典型的递归问题，即大规模问题可以转变成小规模的与原问题形式一样的子问题，诸如此类的问题还有“爬梯子”“斐波那契数列”等等，使用递归最主要是要明确这三点，递归的终止条件，递归终止时的处理办法，提取重复逻辑来减小问题规模。
+
+import copy
+
+class Solution(object):
+    """
+    :type n: int
+    :type k: int
+    :rtype: List[List[int]]
+    """
+
+    def combine(self, n, k):
+
+#                           保存最终结果    存每次结果的临时list    s表示下一轮递归从哪个index开始往下捞数
+        def recursive(n, k, ret=[],        pre=[],                s=1):
+            if n == 0 and k == 0:
+                return ret
+            if k == 1:
+                for i in range(s, n + 1):
+                    pre.append(i)
+                    ret.append(copy.copy(pre))
+                    pre.pop()
+                return ret
+            for i in range(s, n - k + 2):
+                pre.append(i)
+                recursive(n, k - 1, ret, pre, i + 1)
+                pre.pop()
+
+
+        ret = []
+        recursive(n, k, ret)
+
+        return ret
+
+s = Solution()
+x = s.combine(4, 2)
+print x
