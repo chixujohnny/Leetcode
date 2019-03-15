f63037d021149d4ed913bef3e3be06e4bbbb68ad
chixujohnny <1390463349@qq.com>
2019-03-15 17:22:59 +0800

    2019年刷题

--- Leetcode2019/383. 赎金信.py
+++ Leetcode2019/383. 赎金信.py
@@ -10,3 +10,31 @@ class Solution(object):
 
     def canConstruct(self, ransomNote, magazine):
 
+        if len(magazine) < len(ransomNote):
+            return False
+        if len(ransomNote) == 0:
+            return True
+
+        rans_dict = {}
+        for item in ransomNote:
+            if rans_dict.has_key(item) == False:
+                rans_dict[item] = 1
+            else:
+                rans_dict[item] += 1
+
+        for item in magazine:
+            if rans_dict.has_key(item)== True:
+                rans_dict[item] -= 1
+                if rans_dict[item] == 0:
+                    rans_dict.pop(item)
+            if len(rans_dict) == 0:
+                return True
+        return False
+
+s = Solution()
+print s.canConstruct("bg","efjbdfbdgfjhhaiigfhbaejahgfbbgbjagbddfgdiaigdadhcfcj")
+print s.canConstruct('','')
+print s.canConstruct('','a')
+print s.canConstruct('a','b')
+print s.canConstruct('aa','ab')
+print s.canConstruct('aa','aab')
