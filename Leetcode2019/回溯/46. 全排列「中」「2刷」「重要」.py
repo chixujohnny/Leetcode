71a23d7cae9a6afcadb7d83eb2fa92ccecb0ce64
chixujohnny <1390463349@qq.com>
2020-04-27 00:05:22 +0800

    2020年刷题

---
+++ Leetcode2019/回溯/46. 全排列「中」「2刷」「重要」.py
@@ -0,0 +1,85 @@
+# coding: utf-8
+
+# class Solution(object):
+#     def permute(self, nums):
+#         """
+#         :type nums: List[int]
+#         :rtype: List[List[int]]
+#         """
+#
+#         res = []
+#
+#         def helper(nums, path):
+#
+#             if nums == []:
+#                 res.append(path[:])
+#                 return
+#
+#             for i in range(len(nums)):
+#                 path.append(nums[i])
+#                 helper(nums[:i] + nums[i + 1:], path)
+#                 path.pop()
+#
+#         helper(nums, [])
+#
+#         return res
+#
+# s = Solution()
+# print(s.permute([1,2,3]))
+
+
+
+
+
+
+class Solution(object):
+
+    def permute(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+
+        res = []
+
+        def helper(nums, path):
+            if nums == []:
+                res.append(path[:])
+                return
+
+            for i in range(len(nums)):
+                path.append(nums[i])
+                helper(nums[:i]+nums[i+1:], path)
+                path.pop()
+
+            return res
+
+        return helper(nums, [])
+
+
+s = Solution()
+print(s.permute([1,2,3]))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
