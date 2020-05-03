b2ce943c9c4d4b98a52034c9287eae8c8a307a62
chixujohnny <1390463349@qq.com>
2020-05-03 23:37:41 +0800

    2020年刷题

---
+++ Leetcode2019/回溯/131. 分割回文串「中」.py
@@ -0,0 +1,44 @@
+# coding: utf-8
+
+class Solution(object):
+    def partition(self, s):
+        """
+        :type s: str
+        :rtype: List[List[str]]
+        """
+
+        res = []
+
+        #  判断是否为回文串
+        #  回文串定义：该string正着读、反着读是一样的
+        def isPalString(path):
+            readAsc = ''
+            for item in path:
+                readAsc += item
+
+            readDesc = ''
+            for item in reversed(path):
+                readDesc += item
+
+            if readAsc == readDesc:
+                return True
+            else:
+                return False
+
+        def helper(s, path):
+
+            if isPalString(path) == True:
+                res.append(path[:])
+
+            for i in range(1, len(s)):
+                path += path + s[:i]
+                helper(s[i:], path)
+                path = path[:-1]
+
+        helper(s, '')
+
+        return res
+
+
+s = Solution()
+print s.partition('aab')
